8a5a279632f2021e5b3f90f5c55774d38cca95fe
jorenbroekema <joren.broekema@gmail.com>
2023-03-02 21:20:38 +0100

    chore: remove redundant pkg.json sorter script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,3 @@
 module.exports = {
   extends: ['@open-wc/eslint-config', 'eslint-config-prettier'],
-  rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: ['scripts/**/*'],
-      },
-    ],
-  },
 };
