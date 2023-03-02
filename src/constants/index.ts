ddb97a312f0c29f441a0d69d7cb7e1a050279c43
jorenbroekema <joren.broekema@gmail.com>
2023-03-02 21:04:51 +0100

    chore: initial release type definitions

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export * from './BoxShadowTypes';
+export * from './ColorModifierTypes';
+export * from './ColorSpaceTypes';
+export * from './Properties';
+export * from './TokenSetStatus';
+export * from './TokenTypes';
