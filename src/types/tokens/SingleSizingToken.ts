6858c14dcd0a8c2841532fe42f56e79f8f5a150e
razvanpop <rop_pazvan@yahoo.com>
2023-04-24 12:17:40 +0300

    update types

--- src/types/tokens/SingleSizingToken.ts
+++ src/types/tokens/SingleSizingToken.ts
@@ -1,4 +1,9 @@
 import { TokenTypes } from '../../constants/TokenTypes';
 import { SingleGenericToken } from './SingleGenericToken';
 
-export type SingleSizingToken = SingleGenericToken<TokenTypes.SIZING, string>;
+export type SingleSizingToken<Named extends boolean = true, P = unknown> = SingleGenericToken<
+  TokenTypes.SIZING,
+  string,
+  Named,
+  P
+>;
